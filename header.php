62d2650a2ed1a4cb1371828c801704053eb5b8e3
JWizerd <jwodke@outlook.com>
2017-09-28 19:08:42 -0600

    typography and css refinements

--- header.php
+++ header.php
@@ -18,14 +18,12 @@
       </div>
     </nav>
     <div class="row">
-      <div class="col-sm-4 col-sm-offset-4 text-center logo-wrap">
-        <a href="<?php bloginfo('url'); ?>">
-          <div class="logo">
-            <div class="logo-inner">
-              <h1>The Simple Stoic</h1>
-            </div>
-          </div><!-- logo -->
-        </a>
+      <div class="col-sm-6 col-sm-offset-3 text-center logo-wrap">
+        <h1>
+          <a href="<?php bloginfo('url'); ?>">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.png" alt="Simple Stoic: Stoicism Blog">
+          </a>
+        </h1>
       </div>
       <div class="col-sm-6 col-sm-offset-3">
         <h3 class="slogan text-center">Test driven strategies towards financial and mental freedom.</h3>
